0902febe3e5db1ae5a827dbf79dbf82d9388228c
Matrix <rhailu88@gmail.com>
2022-12-06 17:22:20 -0500

    Added get github username for new engineer to test file.

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -1,3 +1,15 @@
 const Engineer = require('../lib/Engineer');
 const Employee = require('../lib/Employee');
 const { test } = require('@jest/globals');
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer('Engineer1');
+
+    expect(typeof(engineer)).toBe('object');
+});
+
+test('gets github username', () => {
+    const engineer = new Engineer('Manager', '1', 'jsmith84@gmail.com', 'Johngithub');
+
+    expect(engineer.github).toBe('Johngithub');
+});
